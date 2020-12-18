12b2cc77efd3af43c39c81c8796eefe00a928c06
CharleyDvs <ing.carlosjaime@outlook.com>
2020-12-17 22:06:33 -0600

    Added css modules

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '@components/Navbar/Navbar';
+
+import styles from './layout.module.css';
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <div className={styles.topmenu}>
       <Navbar />
       {children}
       <footer>This is the footer</footer>
